ea4469d7e853d34ab88ef5c7bac8cfff8dbce551
Leonid Yakubov <leoyakubovdev@gmail.com>
2022-10-22 23:38:15 +0300

    updated procman

--- server/config/connectdb.js
+++ server/config/connectdb.js
@@ -1,7 +1,7 @@
 const mysql = require('mysql2');
 
 const connection = mysql.createConnection({
-  host: 'localhost',
+  host: process.env.CLEARDB_DATABASE_URL || 'localhost',
   user: 'root',
   password: 'test123',
   database: 'feedbackform_db'
